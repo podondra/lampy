6b6a36319fd1dacb36c586535d695c1ffcab81ba
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2019-04-25 12:17:07 +0200

    Greedy experiments

--- lampy/data.py
+++ lampy/data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -24,3 +25,14 @@ def create_dataset(df, target='pm10'):
 
 def train_validation_split(X, y):
     return train_test_split(X, y, test_size=0.5, shuffle=False)
+
+
+def transform_dataset(X, y, window=2):
+    n_rows, n_features = X.shape
+    new_X = np.zeros((n_rows, window * n_features))
+    new_X[:, :n_features] = X
+    for idx in range(1, window):
+        start = n_features * idx
+        end = n_features * (idx + 1)
+        new_X[idx:, start:end] = X[:-idx]
+    return new_X[(window - 1):], y[(window - 1):]
